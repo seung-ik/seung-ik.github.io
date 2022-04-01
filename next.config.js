7a864da9e5deaac7af6cae588aa46b03d9e5ba28
seung-ik <5seungik@traum.biz>
2022-04-01 16:50:07 +0900

    new next for deploy test

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const debug = process.env.NODE_ENV !== 'production'
-const name = 'tradepr'
 const nextConfig = {
-  assetPrefix: !debug ? `/${name}/` : '',
   reactStrictMode: true,
-  distDir: 'build',
-  compiler: {
-    // ssr and displayName are configured by default
-    styledComponents: true,
-  },
-};
+}
 
 module.exports = nextConfig
