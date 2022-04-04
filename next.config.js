c6bd8ebf5d1f33af667ca84f275f62112143d601
seung-ik <5seungik@traum.biz>
2022-04-04 18:44:51 +0900

    Add CDN

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
+module.exports = {
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd
+    ? "https://cdn.statically.io/gh/NaveenDA/seung-ik.github.io/tradepr/"
+    : "",
+};
